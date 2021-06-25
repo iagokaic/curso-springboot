3f7ba33b9c3559e0fa580fbfadd98f11f67dbd04
Iago Kaic <kaiciago2012@hotmail.com>
2021-06-25 11:05:49 -0400

    Projeto criado

---
+++ src/main/java/com/iagokaic/curstosb/CurstosbApplication.java
@@ -0,0 +1,13 @@
+package com.iagokaic.curstosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurstosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurstosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iagokaic/curstosb/CurstosbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagokaic.curstosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurstosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
