51ab315cf9f9fe1ad8566102ca1a623e9db6d5a9
Iago Kaic <kaiciago2012@hotmail.com>
2021-07-01 12:08:26 -0400

    ItemPedido, associação muitos para muitos e outros atributos

---
+++ src/main/java/com/iagokaic/curstosb/entidades/ItemPedido.java
@@ -0,0 +1,75 @@
+package com.iagokaic.curstosb.entidades;
+
+import com.iagokaic.curstosb.entidades.pk.ItemPedidoPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_item_pedido")
+public class ItemPedido implements Serializable {
+
+    @EmbeddedId
+    private ItemPedidoPK id;
+
+    private Integer quantidade;
+    private Double preco;
+
+    public  ItemPedido() {
+    }
+
+    public ItemPedido(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public Pedido getPedido() {
+        return id.getPedido();
+    }
+
+    public void setPedido(Pedido pedido) {
+        id.setPedido(pedido);
+    }
+
+    public Produto getProduto() {
+        return id.getProduto();
+    }
+
+    public void setProduto(Produto produto) {
+        id.setProduto(produto);
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ItemPedido)) return false;
+        ItemPedido that = (ItemPedido) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
